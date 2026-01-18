e835b8066601940ff3970fbe675e155a121d1a80
Estiak Ahmed Sonet <estiakahmedsonet@gmail.com>
2026-01-19 01:39:32 +0600

    Add files via upload
    add or xor

---
+++ add or xor.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+     
+     int a,b,x,y;
+     cin>>a>>b>>x>>y;
+     
+     
+
+
+    }
+
+
+
+
+
+
+    
+}
+
